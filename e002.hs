85ab06fbe17cc0edf4e66f1871ebb205d87dfcd9
quadronick <quadronick@gmail.com>
2019-11-17 04:53:07 +0300

    Add .gitignore. Solve e002.

---
+++ e002.hs
@@ -0,0 +1,10 @@
+module Main where
+
+fib :: [Integer]
+fib = 1 : 1 : zipWith (+) fib (tail fib)
+
+list = sum(takeWhile (< 4000000) [ n | n <-fib, even n])
+
+main :: IO()
+main = do
+  print(list)
