2f4611ca96bb0e70102bde8f6273fb26536c5615
quadronick <quadronick@gmail.com>
2019-11-20 11:39:06 +0300

    Solve e005 problem.

--- 1-10/e005.hs
+++ 1-10/e005.hs
@@ -4,9 +4,10 @@ primes :: Integral a => a -> a -> [a]
 primes a b = takeWhile (<= b) $ dropWhile (< a) $ sieve [2..]
   where sieve (n:ns) = n:sieve [ m | m <- ns, m `mod` n /= 0 ]
 
+ylimit :: Int -> Int
+ylimit x = round( logBase (fromIntegral x) (fromIntegral 20))
 
-
-result = foldr (*) 1 [ x | x <- primes 1 20, y <- [1..truncate(logBase x 20)], x ^ y < 20]
+result = foldr (*) 1 [ x | x <- primes 1 20, y <- [1..ylimit x], x ^ y < 20]
 
 main :: IO()
 main = do
